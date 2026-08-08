5f215d93cc755010a941405b2df410799ada3e96
cristians-12 <cristian.sanchez.casd@gmail.com>
2026-08-08 15:01:44 -0500

    feat: add HomeHeader component and styles for the home screen

---
+++ src/components/organisms/home-header/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function HomeHeader() {
+    return (
+        <View>
+            <View>
+                <Text>Buenos dias, Ricardo</Text>
+            </View>
+        </View>
+    );
+}
