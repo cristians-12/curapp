77dcaeef96d30f888c29f7205243fb9332690ac0
Oscar Ojeda <oscar.vup@gmail.com>
2026-08-12 14:40:29 -0500

    feat: Add QuickActions component and integrate it into HomeTemplate

---
+++ src/components/organisms/quick-actions/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View} from 'react-native';
+import { styles } from './styles';
+
+export default function QuickActions() {
+    return (
+        <View style = {styles.container}>
+            <Text>Acciones Rápidas</Text>
+        </View>
+    );
+}
+
